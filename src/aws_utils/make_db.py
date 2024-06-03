406e645d6cad55a71708267dfc8246c1d0e5c096
Jean Christophe CAZES <cazes.jean.christophe@gmail.com>
2024-06-03 16:46:47 +0200

    upload db to s3 ok

--- src/aws_utils/make_db.py
+++ src/aws_utils/make_db.py
@@ -3,8 +3,18 @@ import pandas as pd
 from s3_utils import aws_sts_login, load_aws_cfg, s3_login, download_file, upload_file
 from datetime import datetime
 import numpy as np
+import os
 
 def process_listing(listing_csv_path):
+    """
+    Process the listing CSV file.
+
+    Args:
+    - listing_csv_path (str): Path to the listing CSV file.
+
+    Returns:
+    - listing_df (pd.DataFrame): Processed DataFrame containing listings data.
+    """
     listing_df = pd.read_csv(listing_csv_path, index_col= 0)
     listing_df['listing_id'] = listing_df.index
     listing_df = listing_df.rename(columns={'productid': 'user_prdtypecode'})
@@ -15,7 +25,13 @@ def process_listing(listing_csv_path):
     listing_df['user'] = 'init_user'
     return(listing_df)
 
-def process_user_table():
+def init_user_table():
+    """
+    Initialize the user table with default data.
+
+    Returns:
+    - user_df (pd.DataFrame): DataFrame containing user data.
+    """
     user_data = {
     'username': ['jc','fred','wilfried','init_user'],
     'first_name': ['jc','fred','wilfried','init_user'],
@@ -24,74 +40,65 @@ def process_user_table():
     }
     user_df = pd.DataFrame(user_data)
     return(user_df)
-
-def load_csv_into_duckdb(csv_path, table_name):
+    
+def create_table_from_pd_into_duckdb(duckdb_connection,pd_df, table_name):
     """
     Loads a CSV file into a DuckDB database.
 
     Args:
-    - csv_path (str): The path to the CSV file to be loaded.
+    - pd_df (pd.DataFrame): The pd.DataFrame to be loaded.
     - table_name (str): The name of the table in DuckDB.
 
     Returns:
     - duckdb_conn: The DuckDB connection.
     """
-    df = pd.read_csv(csv_path)
-    duckdb_conn = duckdb.connect()
-    duckdb_conn.execute(f"CREATE TABLE {table_name} AS SELECT * FROM df")
-    return duckdb_conn
+    duckdb_connection.execute(f"CREATE TABLE {table_name} AS SELECT * FROM pd_df")
 
-def save_duckdb_to_file(duckdb_conn, db_file_path):
+def save_duckdb_to_parquet(duckdb_conn, db_file_path):
     """
-    Saves the DuckDB database to a file.
+    Saves the DuckDB database to a parquetfile.
 
     Args:
     - duckdb_conn: The DuckDB connection.
     - db_file_path (str): The path where the database will be saved.
     """
     duckdb_conn.execute(f"EXPORT DATABASE '{db_file_path}' (FORMAT 'PARQUET')")
-    return duckdb_conn
 
-#Usage
-#aws_config = load_aws_cfg("/home/jc/Workspace/mar24cmlops_rakuten/.aws_config")
+def upload_db_to_s3(aws_config_path, file_path_db, bucket_name):
+    """
+    Uploads a database file to the "db" folder in an S3 bucket.
 
+    Args:
+        aws_config_path (str): Path to the AWS configuration file.
+        file_path_db (str): Path to the database file to upload.
+        bucket_name (str): Name of the S3 bucket.
 
-#sts_session = aws_sts_login(**aws_config)
+    Returns:
+        bool: True if the upload was successful, False otherwise.
+    """
+    # Load AWS configuration from the specified file path
+    aws_config = load_aws_cfg(aws_config_path)
+    
+    # Create an STS session with credentials
+    sts_session = aws_sts_login(**aws_config)
+    
+    # Create an S3 client with the STS session
+    s3_client = s3_login(sts_session)
+    
+    # Define the object name (S3 key) for the uploaded file
+    object_name = f"db/{os.path.basename(file_path_db)}"
+    
+    # Upload the database file to the S3 bucket
+    return upload_file(s3_client, file_path_db, bucket_name, object_name)
 
+# Example Usage (Init of Rakuten DB, saved on S3)
+# listing_df = process_listing('X_train.csv')
+# user_df = init_user_table()
 
-#s3_client = s3_login(sts_session)
-
-#upload_file(s3_client, file_path, bucket, object_name=None)
-
-#download_file(s3_client, 'X_test.csv', 'rakutenprojectbucket', file_path = '/home/jc/Workspace/mar24cmlops_rakuten/X_test.csv')
-#duckdb_conn = load_csv_into_duckdb('/home/jc/Workspace/mar24cmlops_rakuten/X_test.csv', 'listings')
-#save_duckdb_to_file(duckdb_conn, db_file_path = '/home/jc/Workspace/mar24cmlops_rakuten/db.duckdb')
-#duckdb_conn.close()
-
-
-#con = duckdb.connect()
-
-# Requête SQL
-#query = f"SELECT * FROM '{"/home/jc/Workspace/mar24cmlops_rakuten/db.duckdb/listings.parquet"}' LIMIT 10"
-
-# Exécution de la requête
-#result = duckdb_conn.sql(query).fetchall()
-
-# Fermer la connexion
-#con.close()
-
-listing_df = process_listing('X_test.csv')
-user_df = process_user_table()
-
-
-# # Chemin vers le fichier DuckDB
-# db_file_path = '/home/jc/Workspace/mar24cmlops_rakuten/db.duckdb'
-
-# # Connexion à la base de données DuckDB
+# db_file_path = '/home/jc/Workspace/mar24cmlops_rakuten/data/rakuten_db.duckdb'
 # con = duckdb.connect(database=db_file_path, read_only=False)
 
-# # Requête SQL
-# query = f"CREATE TABLE listings2 AS SELECT * FROM df"
+# create_table_from_pd_into_duckdb(con, listing_df, 'fact_listings')
+# create_table_from_pd_into_duckdb(con, user_df, 'dim_user')
 
-# # Exécution de la requête
-# result = con.execute(query).fetchall()
+# con.close()
