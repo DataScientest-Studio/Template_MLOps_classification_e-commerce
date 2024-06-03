82659302c5519404ad45e1c249320fd17d7c6866
Jean Christophe CAZES <cazes.jean.christophe@gmail.com>
2024-06-03 12:25:59 +0200

    changed docstring and modified download function

--- src/aws_utils/s3_utils.py
+++ src/aws_utils/s3_utils.py
@@ -22,19 +22,19 @@ def aws_sts_login(ARN, SessionName):
     return sts_log_session
 
 def s3_login (sts_log_session):
-    s3_client = sts_session.client("s3")
+    s3_client = sts_log_session.client("s3")
     return s3_client
 
 def upload_file(s3_client, file_path, bucket, object_name=None):
     """Upload a file to an S3 bucket
 
-    :param file_name: File to upload
+    :param file_path: File to upload
     :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
+    :param object_name: S3 object name. If not specified then file_path is used
     :return: True if file was uploaded, else False
     """
 
-    # If S3 object_name was not specified, use file_name
+    # If S3 object_name was not specified, use file_path
     
     if object_name is None:
         object_name = os.path.basename(file_path)
@@ -53,21 +53,21 @@ def load_aws_cfg(cfg_path):
     cfg = json.load(cfg_file)
     return cfg
 
-def download_file(s3_client, file_path, bucket, object_name=None):
-    """Download a file to an S3 bucket
+def download_file(s3_client, object_name, bucket, file_path = None):
+    """Download a file from a S3 bucket to file_path
 
-    :param file_name: File to upload
+    :param file_path: File to download
     :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
+    :param object_name: S3 object name. If not specified then file_path is used
     :return: True if file was uploaded, else False
     """
 
-    # If S3 object_name was not specified, use file_name
+    # If file_path was not specified, use current directory 
     
-    if object_name is None:
-        object_name = os.path.basename(file_path)
+    if file_path is None:
+        file_path = os.getcwd() + f'/{object_name}'
 
-    # Upload the file
+    # Download the file
     
     try:
         response = s3_client.download_file(bucket,object_name,file_path)
@@ -77,8 +77,8 @@ def download_file(s3_client, file_path, bucket, object_name=None):
     return True
 
 # How to use:
-# aws_config = load_aws_cfg("/home/jc/Workspace/mar24cmlops_rakuten/.aws_config")
-# sts_session = aws_sts_login(**aws_config)
-# s3_client = s3_login(sts_session)
+#aws_config = load_aws_cfg("/home/jc/Workspace/mar24cmlops_rakuten/.aws_config")
+#sts_session = aws_sts_login(**aws_config)
+#s3_client = s3_login(sts_session)
 #upload_file(s3_client, file_path, bucket, object_name=None)
-#download_file(s3_client, file_path, bucket, object_name=None)
+#download_file(s3_client,'Y_train.csv', 'rakutenprojectbucket')
