780af22ea2b86b079619603c695b3204ec98db3b
Jean Christophe CAZES <cazes.jean.christophe@gmail.com>
2024-06-03 11:27:29 +0200

    added download from s3 functionnality

--- src/aws_utils/s3_utils.py
+++ src/aws_utils/s3_utils.py
@@ -53,8 +53,32 @@ def load_aws_cfg(cfg_path):
     cfg = json.load(cfg_file)
     return cfg
 
+def download_file(s3_client, file_path, bucket, object_name=None):
+    """Download a file to an S3 bucket
+
+    :param file_name: File to upload
+    :param bucket: Bucket to upload to
+    :param object_name: S3 object name. If not specified then file_name is used
+    :return: True if file was uploaded, else False
+    """
+
+    # If S3 object_name was not specified, use file_name
+    
+    if object_name is None:
+        object_name = os.path.basename(file_path)
+
+    # Upload the file
+    
+    try:
+        response = s3_client.download_file(bucket,object_name,file_path)
+    except ClientError as e:
+        logging.error(e)
+        return False
+    return True
+
 # How to use:
-# aws_config = load_aws_cfg(cfg_path)
+# aws_config = load_aws_cfg("/home/jc/Workspace/mar24cmlops_rakuten/.aws_config")
 # sts_session = aws_sts_login(**aws_config)
 # s3_client = s3_login(sts_session)
-# upload_file(s3_client, file_path, bucket, object_name=None)
+#upload_file(s3_client, file_path, bucket, object_name=None)
+#download_file(s3_client, file_path, bucket, object_name=None)
